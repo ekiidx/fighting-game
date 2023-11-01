8b2771f27a865015199d7dfcf17751c9adc3e9c1
ekiidx <info@vuedesign.co>
2023-10-31 20:10:07 -0400

    added sprite animation

--- index.js
+++ index.js
@@ -8,6 +8,26 @@ c.fillRect(0, 0, canvas.width, canvas.height)
 
 const gravity = 0.7
 
+// Background
+const background = new Sprite({
+    position: {
+        x: 0,
+        y: 0
+    },
+    imageSrc: './assets/img/bg.png'
+})
+
+// Fox Sprite
+const fox = new Sprite({
+    position: {
+        x: 700,
+        y: 422
+    },
+    imageSrc: './assets/img/fox.png',
+    scale: 2.75,
+    framesMax: 5
+})
+
 // Player Sprite
 const player = new Fighter({
     position: {
@@ -59,49 +79,14 @@ const keys = {
     }
 }
 
-// rectangle1 = Player, rectangle2 = enemy
-function rectangularCollision({ rectangle1, rectangle2 }) {
-    return (
-        rectangle1.attackBox.position.x + rectangle1.attackBox.width >= rectangle2.position.x && 
-        rectangle1.attackBox.position.x <= rectangle2.position.x + rectangle2.width && 
-        rectangle1.attackBox.position.y + rectangle1.attackBox.height >= rectangle2.position.y && 
-        rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height
-    )
-}
-
-function determineWinner({ player, enemy, timerId }) {
-    clearTimeout(timerId)
-    document.querySelector('#displayText').style.display = 'flex'
-    if(player.health === enemy.health) {
-        document.querySelector('#displayText').innerHTML = 'Tie'
-    } else if (player.health > enemy.health) {
-        document.querySelector('#displayText').innerHTML = 'Player 1 Wins'
-    } else if (enemy.health > player.health) {
-        document.querySelector('#displayText').innerHTML = 'Player 2 Wins'
-    }
-}
-
-let timer = 60
-let timerId
-function decreaseTimer() {
-    
-    if (timer > 0) {
-        timerId = setTimeout(decreaseTimer, 1000) 
-        timer--
-        document.querySelector('#timer').innerHTML = timer
-    }
-
-    if(timer === 0) {
-        determineWinner({ player, enemy, timerId })
-    }
-}
-
 decreaseTimer();
 
 function animate() {
     window.requestAnimationFrame(animate)
     c.fillStyle = 'black'
     c.fillRect(0, 0, canvas.width, canvas.height)
+    background.update()
+    fox.update()
     player.update()
     enemy.update()
 
